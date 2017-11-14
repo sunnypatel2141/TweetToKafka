1480a91c80dfafbd530e3643785547e50328f4d0
sunnypatel2141 <sanipatel2141@hotmail.com>
2017-11-13 21:27:00 -0500

    Multithreading capabilitie added (unused)

--- src/TwitterStream.java
+++ src/TwitterStream.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
+
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -15,26 +16,49 @@ import twitter4j.conf.ConfigurationBuilder;
  */
 public class TwitterStream 
 {	
-	private static List<Tweets> allTweets = new ArrayList<>();
+//	static class SimpleThread extends Thread 
+//	{
+//		QueryResult result;
+//		
+//	    public SimpleThread(QueryResult result) 
+//	    {
+//	    		this.result = result;
+//	    }
+//	    public void run() 
+//	    {
+//	    		System.out.println("Starting thread ... " + getName());
+//	    		List<Status> tweets = result.getTweets();
+//            for (Status tweet : tweets) {
+//            		Tweets tweetObj = new Tweets(tweet.getUser().getScreenName(), 
+//            				tweet.getText(), tweet.getCreatedAt().toString());
+//            		allTweets.add(tweetObj);
+//            }
+//	    }
+//	}
+	
+	private static Vector<Tweets> allTweets = new Vector<>();
 
 	public static void main(String[] args) throws TwitterException, IOException
 	{
-		if (args.length != 5) {
+		if (args.length != 6) {
 			System.out.println
         			("This application requires five arguments: Consumer Key, Consumer Secret, "
-            		+ "Access Token, Access Token Secret, and Search Value.");
+            		+ "Access Token, Access Token Secret, Search Value, and File Name.");
             System.exit(-1);
         }
 		
 		String keyword = args[4];
+		String filename = args[5];
 		
 		Logging.print("Keyword: " + keyword);
 		
-		ConfigurationBuilder cb = assignAccessParams(args);
-		Twitter twitter = getInstance(cb);
+		TwitterStream ts = new TwitterStream();
+		
+		ConfigurationBuilder cb = ts.assignAccessParams(args);
+		Twitter twitter = ts.getInstance(cb);
 		
         try {
-        		accumulateAllTweets(twitter, keyword);
+        		ts.accumulateAllTweets(twitter, keyword);
         } catch (TwitterException te) {
             te.printStackTrace();
             Logging.print("Failed to search tweets: " + te.getMessage());
@@ -42,10 +66,12 @@ public class TwitterStream
         
         Logging.print("Finished retrieving tweets...");
         
-        instantiateKafkaWriter(keyword);
+//        ts.printContents();
+        
+        ts.instantiateKafkaWriter(keyword, filename);
 	}
 
-	private static ConfigurationBuilder assignAccessParams(String[] args)
+	private ConfigurationBuilder assignAccessParams(String[] args)
 	{
 		Logging.print("Assigning configs...");
 		ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -57,26 +83,31 @@ public class TwitterStream
 		return cb;
 	}
 	
-	private static Twitter getInstance(ConfigurationBuilder cb) 
+	private Twitter getInstance(ConfigurationBuilder cb) 
 	{
 		Logging.print("Create Twitter instance...");
 		TwitterFactory tf = new TwitterFactory(cb.build());
 		return tf.getInstance();
 	}
 	
-	private static void instantiateKafkaWriter(String topic)
+	private void instantiateKafkaWriter(String topic, String filename)
 	{
 		Logging.print("Instantiate Kafka Writer...");
 		KafkaWriter kafkaWriter = new KafkaWriter();
         kafkaWriter.setTopic(topic);
         kafkaWriter.setTweets(allTweets);
+        kafkaWriter.setFilename(filename);
         kafkaWriter.tweetToKafka();
 	}
 	
-	private static void accumulateAllTweets(Twitter twitter, String topic) throws TwitterException
+	private void accumulateAllTweets(Twitter twitter, String topic) throws TwitterException
 	{
         Query query = new Query(topic);
         QueryResult result;
+//        ArrayList<SimpleThread> arrThreads = new ArrayList<SimpleThread>();
+        
+        long start = System.currentTimeMillis();
+        
         do {
             result = twitter.search(query);
             List<Status> tweets = result.getTweets();
@@ -86,9 +117,53 @@ public class TwitterStream
             		allTweets.add(tweetObj);
             }
         } while ((query = result.nextQuery()) != null);
+        
+//        do {
+//        		result = twitter.search(query);
+////        		SimpleThread st = new SimpleThread(result);
+////        		st.start();
+////        		arrThreads.add(st);
+//        		
+//        		if (arrThreads.size() % 10 == 0)
+//        		{
+//        			try
+//					{
+//        					//sleep for five seconds every 10 thread
+//						Thread.sleep(5000);
+//					} catch (InterruptedException e)
+//					{
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//        		}
+//        } while ((query = result.nextQuery()) != null);
+        
+        
+        long end = System.currentTimeMillis();
+        
+        System.out.println("Total time: " + (end - start));
+//        
+//        try
+//        {
+//        		for (int i = 0; i < arrThreads.size(); i++) 
+//    	       {
+//        			arrThreads.get(i).join(); 
+//    	       }
+//        } catch (InterruptedException e)
+//        {
+//        		e.printStackTrace();
+//        }
 	}
 	
-	public List<Tweets> getAllTweets()
+	public void printContents()
+	{
+		for (Tweets tweet : allTweets)
+		{
+			System.out.println(tweet.getCreatedAt() + "..." + tweet.getTweet() + "..." + tweet.getUser());
+		}
+	}
+	
+	public Vector<Tweets> getAllTweets()
 	{
 		return allTweets;
 	}
